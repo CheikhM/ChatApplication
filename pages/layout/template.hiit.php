741fca82c04ba3032fee720c7b459e6debcb8c09
mycheikhelmoctar <elmoctar.yehdhih@gmail.com>
2019-08-06 20:27:21 +0100

    static urls removed

--- pages/layout/template.hiit.php
+++ pages/layout/template.hiit.php
@@ -6,21 +6,21 @@
     <title>HiiT Consulting Chat Room</title>
 
     <!-- style files -->
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1/scripts/ChatApplication/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1/scripts/ChatApplication/assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1/scripts/ChatApplication/assets/css/chat.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/chat.css">
     <!-- end style files  -->
 
     <!-- script files -->
-        <script src="http://127.0.0.1/scripts/ChatApplication/assets/js/bootstrap.min.js"></script>
-        <script src="http://127.0.0.1/scripts/ChatApplication/assets/js/popper.min.js"></script>
-        <script src="http://127.0.0.1/scripts/ChatApplication/assets/js/jquery-3.4.1.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/jquery-3.4.1.min.js"></script>
     <!-- end script files -->
 
 </head>
 <body>
 <?= $content; ?>
 
-<script src="http://127.0.0.1/scripts/ChatApplication/assets/js/script.js"></script>
+<script src="assets/js/script.js"></script>
 </body>
 </html>
